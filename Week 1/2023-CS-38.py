f7f80e59a1bf7ddc5c24e35eaa8c333cb9b56f05
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-09-08 18:32:20 +0500

    Task 2

--- Week 1/2023-CS-38.py
+++ Week 1/2023-CS-38.py
@@ -18,3 +18,28 @@ if indices:
 else:
     print("Number Not found.")
 
+###################################################################################
+
+# Task 2
+def SearchB(Arr, x):
+    indices = []
+    for i in range(len(Arr)):
+        if Arr[i] == x:
+            indices.append(i)
+        elif Arr[i] > x:
+            break
+    return indices
+Arr = [22,2,1,7,11,13,5,2,9]
+Arr.sort()
+x = int(input("Enter the number: "))
+indices =  SearchB(Arr, x)
+if indices:
+    print ("Index: " , end = "")
+    for i in range(len(indices)):
+        if i == len(indices) - 1:
+            print(indices[i])
+        else:
+            print(indices[i] , end =",")
+else:
+    print("Number Not found.")
+
