73f3aced958d89d6818f33f15b631c7f6f036b0b
Zaeem-Shah3321 <zaeemshah58@gmail.com>
2024-09-08 18:31:09 +0500

    Task 1

---
+++ Week 1/2023-CS-38.py
@@ -0,0 +1,20 @@
+# Task 1
+def SearchA(Arr, x):
+    indices = []
+    for i in range(len(Arr)):
+        if Arr[i] == x:
+            indices.append(i)
+    return indices
+Arr = [22,2,1,7,11,13,5,2,9]
+x = int(input("Enter the number: "))
+indices =  SearchA(Arr, x)
+if indices:
+    print ("Index: " , end = "")
+    for i in range(len(indices)):
+        if i == len(indices) - 1:
+            print(indices[i])
+        else:
+            print(indices[i] , end =",")
+else:
+    print("Number Not found.")
+
