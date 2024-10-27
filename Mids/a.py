d1f38c95a7163ae93679ee4d193d7c3946782ace
Zaeem-Shah3321 <zaeembukhari58@gmail.com>
2024-10-27 11:46:37 +0500

    Work

--- Mids/a.py
+++ Mids/a.py
@@ -1,3 +1,7 @@
-def __init__(self,data):
-    self.data = data
-    self.next = None
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+class LinedList:
+    def __init__(self):
+        self.head = None
